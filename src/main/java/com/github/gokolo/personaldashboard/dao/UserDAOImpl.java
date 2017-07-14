7dc99584aaf27677c1e3451fe80339f040b71e3b
Grace Okolo <Grace_Okolo@epam.com>
2017-07-14 12:03:15 +0200

    UserDAO save, findall, find by id

--- src/main/java/com/github/gokolo/personaldashboard/dao/UserDAOImpl.java
+++ src/main/java/com/github/gokolo/personaldashboard/dao/UserDAOImpl.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.github.gokolo.personaldashboard.MysqlConn;
@@ -12,41 +13,44 @@ import com.github.gokolo.personaldashboard.dto.UserDTO;
 
 public class UserDAOImpl implements UserDAO {
     private final Connection conn = MysqlConn.connect();
+    private Statement statement;
+    private PreparedStatement preparedStatement;
     private ResultSet resultSet;
-    private List<UserDTO> userCollection;
+    private UserDTO user;
+    private int rowsInserted;
 
     @Override
-    public int save(UserDTO user) {
+    public int save(final UserDTO user) {
 
         try {
-            PreparedStatement stmt = conn.prepareStatement(
+            preparedStatement = conn.prepareStatement(
                     "INSERT INTO user (name,username,email,password,birthday,gender,role,address_id) VALUES (?,?,?,?,?,?,?,?,?)");
-            stmt.setString(2, user.getName());
-            stmt.setString(3, user.getUsername());
-            stmt.setString(4, user.getEmail());
-            stmt.setString(5, user.getPassword());
-            stmt.setDate(6, user.getBirthday());
-            stmt.setString(7, user.getGender().name());
-            stmt.setString(7, user.getRole().name());
-            stmt.setInt(1, user.getAddress().getId());
-            stmt.execute();
-
-        } catch (SQLException ex) {
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getUsername());
+            preparedStatement.setString(4, user.getEmail());
+            preparedStatement.setString(5, user.getPassword());
+            preparedStatement.setDate(6, user.getBirthday());
+            preparedStatement.setString(7, user.getGender().name());
+            preparedStatement.setString(7, user.getRole().name());
+            preparedStatement.setInt(1, user.getAddress().getId());
+            preparedStatement.execute();
+            rowsInserted = preparedStatement.executeUpdate();
+        } catch (final SQLException ex) {
             // handle any errors
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
         }
-        return 0;
+        return rowsInserted;
     }
 
     @Override
     public List<UserDTO> findAll() {
+        List<UserDTO> userCollection = new ArrayList<>();
         try {
-            Statement stmt = conn.createStatement();
-            resultSet = stmt.executeQuery("SELECT *FROM user");
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM user");
             while (resultSet.next()) {
-                UserDTO user = new UserDTO();
                 user.setId(resultSet.getInt("id"));
                 user.setName(resultSet.getString("name"));
                 // user.setAddress(address);
@@ -58,7 +62,7 @@ public class UserDAOImpl implements UserDAO {
                 // role
                 userCollection.add(user);
             }
-        } catch (SQLException e) {
+        } catch (final SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
@@ -66,19 +70,38 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public UserDTO findById(int id) {
+    public UserDTO findById(final int id) {
+        try {
+            preparedStatement = conn.prepareStatement("SELECT *FROM user WHERE id = ?");
+            preparedStatement.setInt(1, user.getId());
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                // user.setAddress(address);
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                user.setBirthday(resultSet.getDate("birthday"));
+                user.setEmail(resultSet.getString("email"));
+                // gender
+                // role
+            }
+        } catch (final SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    @Override
+    public UserDTO modify(final UserDTO user) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public UserDTO modify(UserDTO user) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void delete(UserDTO user) {
+    public void delete(final UserDTO user) {
         // TODO Auto-generated method stub
 
     }
