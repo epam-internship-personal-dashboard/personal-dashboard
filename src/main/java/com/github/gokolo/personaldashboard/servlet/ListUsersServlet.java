eec830301ccce4a2518b41e7c03c5aebbf41e0bf
Grace Okolo <Grace_Okolo@epam.com>
2017-07-24 15:43:22 +0200

    List all the users in the database to a web page

---
+++ src/main/java/com/github/gokolo/personaldashboard/servlet/ListUsersServlet.java
@@ -0,0 +1,38 @@
+package com.github.gokolo.personaldashboard.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.gokolo.personaldashboard.dao.UserDAO;
+import com.github.gokolo.personaldashboard.dao.UserDAOImpl;
+import com.github.gokolo.personaldashboard.dto.UserDTO;
+
+@WebServlet(name = "ListUsersServlet", urlPatterns = "/xxx")
+public class ListUsersServlet extends HttpServlet {
+    private UserDAO userDAO = new UserDAOImpl();
+
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html");
+        List<UserDTO> userCollection = userDAO.findAll();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<!DOCTYPE html>").append("<head>").append("<title>").append("All Users").append("</title>")
+                .append("</head>").append("<body>");
+
+        for (UserDTO user : userCollection) {
+            sb.append("<p>").append(user.getName()).append("</p>");
+        }
+        sb.append("</body>").append("</html>");
+
+        PrintWriter writer = response.getWriter();
+        writer.println(sb.toString());
+    }
+
+}
