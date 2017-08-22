9b6d0841d5cfdd4400cfb5a65664c17b4b0fcb9c
Grace Okolo <grace.okolo15@gmail.com>
2017-08-22 13:43:27 +0200

    Angular user service for CRUD

--- src/main/webapp/resources/js/services/users.services.js
+++ src/main/webapp/resources/js/services/users.services.js
@@ -2,32 +2,57 @@
     'use strict';
 
     angular.module('pd.services').service('pdUserService', function($http){
-    	this.getAll = function(noErrorCallback, errorCallback){
+    	
+    	this.getAll = function(successCallback, errorCallback){
     		$http.get('/api/users').then(function(responseObj){
-    			noErrorCallback(responseObj.data);
-    		}, 
+    			successCallback(responseObj.data);
+    		},
     		function(errorObj) {
     			errorCallback(errorObj.data);
     		});
     	}
     	
-    	this.getUser = function(id){
+    	this.getUser = function(id, successCallback, errorCallback){
     		$http.get('/api/users/'+id).then(function(responseObj){
-    			return responsObj.data;
+    			successCallback(responseObj.data);
     		},
     		function(errorObj) {
-    			return errorObj.data;
+    			errorCallback(errorObj.data);
+    		});
+    	}
+     
+    	this.saveUser = function(user, successCallback, errorCallback){
+    		var request = {
+        			method: 'POST',
+        			url: '/api/users',
+        			data: user
+        	}
+    		$http(request).then(function(responseObj){
+    			successCallback(responseObj.data);
+    		},
+    		function(errorObj) {
+    			errorCallback(errorObj.data);
     		});
     	}
     	
-    	this.saveUser = function(user){
-    		$http('').then(function(responseObj){
-    			return responsObj.data;
+    	this.deleteUser =  function(id, successCallback, errorCallback){
+    		$http.delete('/api/users/'+id).then(function(responseObj){
+    			successCallback(responseObj.data);
     		},
     		function(errorObj) {
-    			return errorObj.data;
+    			errorCallback(errorObj.data);
     		});
     	}
+    	
+    	this.updateUser =  function(id, successCallback, errorCallback){
+    		$http.put('/api/users/'+id).then(function(responseObj){
+    			successCallback(responseObj.data);
+    		},
+    		function(errorObj) {
+    			errorCallback(errorObj.data);
+    		});
+    	}
+    	
     });
 
 }());
