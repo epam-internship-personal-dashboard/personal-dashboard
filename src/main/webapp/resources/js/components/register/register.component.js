8eb4a8b45d015e79562ddbe83e59579a02d9dfe6
Grace Okolo <grace.okolo15@gmail.com>
2017-08-24 15:21:22 +0200

    Created register page component

--- src/main/webapp/resources/js/components/register/register.component.js
+++ src/main/webapp/resources/js/components/register/register.component.js
@@ -1,11 +1,32 @@
 (function (app) {
     'use strict';
 
-    var cmpt = {
+    var component = {
         templateUrl: '/views/register.view.html',
-        controllerAs: 'ctrl'
+        controllerAs: 'ctrl',
+        controller: function (pdUserService, $location) {
+        	var ctrl = this;
+        	
+        	var successCallback = function(){
+        		ctrl.msg = false;
+        		$location.url('');
+        	};
+        	var errorCallback = function(){
+        		ctrl.msg = true;
+        	};
+        	
+        	ctrl.user = {
+        		role: 'ROLE_USER',
+        		enabled: 1
+        	};
+        	
+        	ctrl.submit = function () {
+        		pdUserService.save(ctrl.user, successCallback, errorCallback);
+        	};
+        	
+        }
     };
 
-    app.component('pdRegister', cmpt);
+    app.component('pdRegister', component);
 
 }(angular.module('pd.app')));
--- src/main/webapp/resources/js/services/users.services.js
+++ src/main/webapp/resources/js/services/users.services.js
@@ -1,10 +1,10 @@
 (function () {
     'use strict';
-
+    var path = 'api/users';
     angular.module('pd.services').service('pdUserService', function($http){
 
         this.getAll = function(successCallback, errorCallback) {
-            $http.get('/api/users').then(function(responseObj) {
+            $http.get(path).then(function(responseObj) {
                 successCallback(responseObj.data);
             }, function(errorObj) {
                 errorCallback(errorObj.data);
@@ -12,17 +12,17 @@
         };
 
         this.getUser = function(id, successCallback, errorCallback) {
-            $http.get('/api/users/'+id).then(function(responseObj) {
+            $http.get(path+id).then(function(responseObj) {
                 successCallback(responseObj.data);
             }, function(errorObj) {
                 errorCallback(errorObj.data);
             });
         };
 
-        this.saveUser = function(user, successCallback, errorCallback) {
+        this.save = function(user, successCallback, errorCallback) {
             var request = {
                 method: 'POST',
-                url: '/api/users',
+                url: path,
                 data: user
             };
 
@@ -34,15 +34,15 @@
         };
 
         this.deleteUser =  function(id, successCallback, errorCallback){
-            $http.delete('/api/users/'+id).then(function(responseObj) {
+            $http.delete(path+id).then(function(responseObj) {
                 successCallback(responseObj.data);
             }, function(errorObj) {
                 errorCallback(errorObj.data);
             });
         };
 
-        this.updateUser =  function(id, successCallback, errorCallback) {
-            $http.put('/api/users/' + id).then(function(responseObj) {
+        this.updateUser =  function(id, user, successCallback, errorCallback) {
+            $http.put(path + id, user).then(function(responseObj) {
                 successCallback(responseObj.data);
             }, function(errorObj) {
                 errorCallback(errorObj.data);
